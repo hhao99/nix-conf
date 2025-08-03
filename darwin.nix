79c1d94260e1021d5cf7c3e03eab1befd95181b6
hhao99 <hhao99@gmail.con>
2025-08-02 22:17:52 -0500

    update darwin.nix

--- darwin.nix
+++ darwin.nix
@@ -1,19 +1,11 @@
 { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      
-      # Necessary for using flakes on this system.
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      # system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
+    
       system.stateVersion = 6;
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+
+      users.users.jijiuhao.home = "/Users/jijiuhao";
+
+      programs.zsh.enable = true;
     }
